0f0d0e103d805a065326f55a788effea7a762590
Yadullah Duman <yadullah.duman@maibornwolff.de>
2021-11-29 20:39:24 +0100

    feat(auth): create users/current route with dummy response

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const PORT = 3001;
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/current", (req, res) => {
+  res.send("Hello World.");
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
