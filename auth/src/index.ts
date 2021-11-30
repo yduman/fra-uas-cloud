69cf9fe985037149693ab5e96016400828cfcedf
Yadullah Duman <yadullah.duman@maibornwolff.de>
2021-11-30 15:01:48 +0100

    refactor(auth): extract router

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
+import { currentUserRouter } from "./routes/currentUser";
+
 const PORT = 3001;
 const app = express();
-app.use(express.json());
 
-app.get("/api/users/current", (req, res) => {
-  res.send("Hello World.");
-});
+app.use(express.json());
+app.use(currentUserRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
